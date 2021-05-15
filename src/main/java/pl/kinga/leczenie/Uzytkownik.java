c35ff8a3ba737b39d9442e9725e1bcbf59ffb006
Kinga Paszkiewicz <kingaa.kulig@gmail.com>
2021-05-15 19:24:12 +0200

    rozwiazanie zad. Etap 1

---
+++ src/main/java/pl/kinga/leczenie/Uzytkownik.java
@@ -0,0 +1,51 @@
+package pl.kinga.leczenie;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class Uzytkownik {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String imie;
+    private String nazwisko;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate data_urodzenia;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public LocalDate getData_urodzenia() {
+        return data_urodzenia;
+    }
+
+    public void setData_urodzenia(LocalDate data_urodzenia) {
+        this.data_urodzenia = data_urodzenia;
+    }
+}
